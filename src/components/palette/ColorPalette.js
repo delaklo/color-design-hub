fca09c147a5215722b74791150933cdcfedcff7e
delaklo <delaklovp@gmail.com>
2022-07-18 20:01:24 +0300

    its not done yet

--- src/components/palette/ColorPalette.js
+++ src/components/palette/ColorPalette.js
@@ -7,9 +7,13 @@ const ColorPalette = ()=>{
     return(
         <div>
             <div className="main">              
-        <h1>Newest Color Palettes</h1>
-        <Palette color1="blue" color2={"purple"} color3={"black"} color4={"yellow"} color5={"red"} color6={"green"}/>
-        <Palette />
+        <h1>Newest Color Combinations</h1>
+        <Palette color1="#c03221" color2="#f2d0a4" color3="#545e75"
+         color4="#3f826d" color5="#f7f7ff" color6="#D61C4E"/>
+        <Palette color1="#FFF9CA" color2="#E7F6F2" color3="#B2A4FF"
+         color4="#2C3333" color5="#A5C9CA" color6="#FFB4B4"/>
+        <Palette color1="#F7EDDB" color2="#DAE2B6" color3="#DFE8CC"
+         color4="#CCD6A6" color5="#F5EDDC" color6="#DEE5D0"/>
     </div>
 </div>
     )
--- src/components/palette/Palette.js
+++ src/components/palette/Palette.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Palette.css";
 import "../../App.css";
 
 const Palette = ({color1, color2, color3, color4, color5, color6})=>{
+    
+    const style1 = {backgroundColor: color1, height: "100%", width: "14%"};
+    const style2 = {backgroundColor: color2, height: "100%", width: "14%"};
+    const style3 = {backgroundColor: color3, height: "100%", width: "14%"};
+    const style4 = {backgroundColor: color4, height: "100%", width: "14%"};
+    const style5 = {backgroundColor: color5, height: "100%", width: "14%"};
+    const style6 = {backgroundColor: color6, height: "100%", width: "14%"};    
 
-    const style1 = {backgroundColor: color1};
-    const style2 = {backgroundColor: color2};
-    const style3 = {backgroundColor: color3};
-    const style4 = {backgroundColor: color4};
-    const style5 = {backgroundColor: color5};
-    const style6 = {backgroundColor: color6};
+    const colors = [{style: style1}, {style: style2}, {style: style3}, {style: style4}, 
+        {style: style5}, {style: style6}];
+
+    const [copied, setCopied] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
+
+    function copy(color){
+        navigator.clipboard.writeText(color);
+        setCopied(true);
+        setTimeout(()=> setCopied(false), 1000)
+    }
     
     return(
         <div>             
-        <div className="content">
+        <div className={darkMode ? "dark-content" : "content"}>
+            <div className="top">
+            <h3>Click on color to copy color code to the clipboard</h3>
+            <div className="btn-dark" onClick={()=>setDarkMode(!darkMode)}>darkMode</div>
+            </div>
             <div className="palette">
-            <div className="color1" style={style1}>1</div>
-            <div className="color2" style={style2}>2</div>
-            <div className="color3" style={style3}>3</div>
-            <div className="color4" style={style4}>4</div>
-            <div className="color5" style={style5}>5</div>
-            <div className="color6" style={style6}>6</div>
-           </div>
+            {colors.map((el)=>{
+                return(
+                    <div style={el.style} onClick={()=>copy(el.style.backgroundColor)}></div>
+                )
+            })}
+          </div>
+          {copied && (<div className="copied">Copied!</div>)}
     </div>
 </div>
     )
